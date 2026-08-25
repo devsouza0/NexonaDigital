29c795b55142155f3ba231ce7f27ddb3ab2e2013
Encanto Kids <viniciusgsouza200600@gmail.com>
2026-08-25 00:51:52 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
+const menuMobile = document.getElementById("menuMobile");
+const nav = document.getElementById("nav");
 
+menuMobile.addEventListener("click", () => {
+    nav.classList.toggle("active");
+
+    const icon = menuMobile.querySelector("i");
+
+    if (nav.classList.contains("active")) {
+        icon.classList.remove("fa-bars");
+        icon.classList.add("fa-xmark");
+    } else {
+        icon.classList.remove("fa-xmark");
+        icon.classList.add("fa-bars");
+    }
+});
+
+document.querySelectorAll(".nav a").forEach(link => {
+    link.addEventListener("click", () => {
+        nav.classList.remove("active");
+
+        const icon = menuMobile.querySelector("i");
+        icon.classList.remove("fa-xmark");
+        icon.classList.add("fa-bars");
+    });
+});
